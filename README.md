3e0103742029bee92f9279021968a5202ffe1777
Red Skull <bradfordmichael7@gmail.com>
2019-12-11 22:46:03 +0000

    added moment js and its functionality to display date and color code times and also updated readme

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 permalink: /index.html
 
 # Day-Planner
-A local storage daily calendar that updates with time of day
+​
+#### Day-Planner, 12/11/2019
+​
+#### By Lawrence Kirk
+​
+## Description
+A local storage daily calendar that updates with time of day. Can save and update tasks and color codes time slots based on whether they are in the future or the past.
+​
+## Features
+* Users can input information into each time slot textarea.
+* Users can save inputted information by pressing the save button. Information is stored locally. 
+* All inputted information is loaded at the time the application is opened.
+* Users can see the date at the top of the application 
+* Users can see a color-coded layout of what is past, current, and yet to come.​
+​
+## Future Commits
+​
+Styles will be polished to give a more refined look. As is now, application is fully functional but is not totally appealing to the eye. 
+​
+## Technologies Used
+​
+HTML
+​
+CSS
+​
+Bootstrap
+
+Javascript
+
+jQuery
+
+Moment.js
--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,41 @@ $(document).ready(function() {
     let storedData = [];
 
     const init = () => {
+        $('#day').append('<p id="dayText"></p>');
+        $('#dayText').text(moment().format('dddd MMMM Do'));
+        if (moment().format('a') === 'am') {
+            $('#3').css('background-color', 'green');
+            for (let i = 4; i < 9; i++) {
+                $('#' + i).css('background-color', 'green');
+            }
+            for (let i = 0; i , 3; i++) {
+                if (parseInt(moment().format('h')) - 8 > i) {
+                    $('#' + i).css('background-color', 'lightgray');
+                } else if (parseInt(moment().format('h')) - 8 === i) {
+                    $('#' + i).css('background-color', 'red');
+                } else if (parseInt(moment().format('h')) - 8 < i) {
+                    $('#' + i).css('background-color', 'green');
+                }
+            }
+        } else if (moment().format('a') === 'pm') {
+            for (let i = 0; i < 3; i++) {
+                $('#' + i).css('background-color', 'lightgray');
+            }
+            if (parseInt(moment().format('h') === 12)) {
+                $('#3').css('background-color', 'red');
+            } else {
+                $('#3').css('background-color', 'lightgray');
+                for (let i = 4; i < 9; i++) {
+                    if (parseInt(moment().format('h')) + 3 > i) {
+                        $('#' + i).css('background-color', 'lightgray');
+                    } else if (parseInt(moment().format('h')) + 3 === i) {
+                        $('#' + i).css('background-color', 'red');
+                    } else if (parseInt(moment().format('h')) + 3 < i) {
+                        $('#' + i).css('background-color', 'green');
+                    }
+                }
+            }
+        }
         storedData = JSON.parse(localStorage.getItem('localStor'));
         for (let i = 0; i < storedData.length; i++) {
             if (storedData[i] !== null) {
@@ -14,7 +49,47 @@ $(document).ready(function() {
         locStor = storedData;
     }
 
+    const checkTime = () => {
+        console.log('test');
+        $('#dayText').text(moment().format('dddd MMMM Do'));
+        if (moment().format('a') === 'am') {
+            $('#3').css('background-color', 'green');
+            for (let i = 4; i < 9; i++) {
+                $('#' + i).css('background-color', 'green');
+            }
+            for (let i = 0; i , 3; i++) {
+                if (parseInt(moment().format('h')) - 8 > i) {
+                    $('#' + i).css('background-color', 'lightgray');
+                } else if (parseInt(moment().format('h')) - 8 === i) {
+                    $('#' + i).css('background-color', 'red');
+                } else if (parseInt(moment().format('h')) - 8 < i) {
+                    $('#' + i).css('background-color', 'green');
+                }
+            }
+        } else if (moment().format('a') === 'pm') {
+            for (let i = 0; i < 3; i++) {
+                $('#' + i).css('background-color', 'lightgray');
+            }
+            if (parseInt(moment().format('h') === 12)) {
+                $('#3').css('background-color', 'red');
+            } else {
+                $('#3').css('background-color', 'lightgray');
+                for (let i = 4; i < 9; i++) {
+                    if (parseInt(moment().format('h')) + 3 > i) {
+                        $('#' + i).css('background-color', 'lightgray');
+                    } else if (parseInt(moment().format('h')) + 3 === i) {
+                        $('#' + i).css('background-color', 'red');
+                    } else if (parseInt(moment().format('h')) + 3 < i) {
+                        $('#' + i).css('background-color', 'green');
+                    }
+                }
+            }
+        }
+    }
+
     init();
+
+    setInterval(checkTime, 60000);
 
     $('button').on('click', function() {
         if ($(this).parent().siblings('.textarea').children('textarea').val() === '') {
